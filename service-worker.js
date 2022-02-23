8de8b30a3b897391d19570e52f78169daf9c8a89
Stray <strayfrozen@yahoo.com>
2022-02-23 14:38:15 -0600

    activated service worker

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+const APP_PREFIX = 'Budget-App';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
     "/index.html",
     "public/css/styles.css",
@@ -7,7 +11,34 @@ const FILES_TO_CACHE = [
     "public/js/index.js"
   ];
 
-self.addEventListener('install', function (e) {
 
-})
 
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+  
+ 
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+       
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        })
+      
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        }));
+      })
+    );
+  });
