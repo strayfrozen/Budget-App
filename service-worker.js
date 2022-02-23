e4018f4c19f9bbf8413f2dcf62d2a3cbfe1a50fa
Stray <strayfrozen@yahoo.com>
2022-02-23 14:27:07 -0600

    updated service worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+    "/index.html",
+    "public/css/styles.css",
+    "/manifest.json",
+    "public/icons/icon-128x128.png",
+    "public/js/idb.js",
+    "public/js/index.js"
+  ];
+
+self.addEventListener('install', function (e) {
+
+})
+
