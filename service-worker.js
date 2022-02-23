3a40f20b0118020072f463976da10b5ead557e60
Stray <strayfrozen@yahoo.com>
2022-02-23 15:14:25 -0600

    updated files_to_cache

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,22 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/index.html",
-    "public/css/styles.css",
-    "/manifest.json",
-    "public/icons/icon-128x128.png",
-    "public/js/idb.js",
-    "public/js/index.js"
+    "/",
+    "./index.html",
+    "./css/styles.css",
+    "./js/idb.js",
+    "./js/index.js",
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
   ];
+  
 
 
 
